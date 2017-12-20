4bc057e02ca68e04fa22e6e1dd76b98f81c89923
Brennan Butler <bbutler522@gmail.com>
2017-12-20 19:02:40 +0000

    create new project cards, display them on the projects page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'project/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'projects', to: 'project#index'
+  post 'projects', to: 'project#create'
 end
